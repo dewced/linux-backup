54d753a3f480bb4f74c2400edfde79c609e8abae
CEDRIC DE WITTE <cedric.dewitte.y2354@student.hogent.be>
2018-12-30 22:51:15 +0100

    Add statements to copy different folders

--- backupscript.sh
+++ backupscript.sh
@@ -3,7 +3,7 @@
 # Linux Automated Backup Script
 # Version: 1.0
 # Script by: Cedric De Witte - Kwinten Braet - Sam Strobbe
-# Email: info@cedricdewitte.Be
+# Support: info@cedricdewitte.be - info@kwintenbraet.be
 
 ########################################
 # Please fill in your relevant details #
@@ -45,33 +45,36 @@ TYPE=1
 echo 'Checking hostsystem...'
 
 
-function check_apache {
+function check_webapp {
 
 curl -I localhost > server.txt
 
-if grep -q "Apache" server.txt; then 
-	echo Apache!;
-	check_os
+if grep -q "apache" server.txt; then 
+	echo Apache is being used!;
+	check_os_apache
+elif grep -q "nginx" server.txt; then 
+	echo Nginx is being used!;
+	nginx_folders
 else
-	echo Not found!
+	echo No Webserver found!
 fi
 }
 
-function check_os {
+function check_os_apache {
 cat /proc/version >> server.txt
 cat /etc/redhat-release >> server.txt
 
 if grep -q "Debian" server.txt; then 
-	echo Debian is used!;
-	folders_debian;
+	echo Debian is being used!;
+	apache_folders_debian;
 
 elif grep -q "Ubuntu" server.txt; then 
-	echo Ubuntu is used!;
-	folders_ubuntu;
+	echo Ubuntu is being used!;
+	apache_folders_ubuntu;
 
 elif grep -q "CentOS" server.txt; then 
-	echo CentOS is used!;
-	folders_centos;
+	echo CentOS is being used!;
+	apache_folders_centos;
 
 else 
 	echo OS not supported!
@@ -79,7 +82,29 @@ fi
 	
 }
 
-function folders_debian {
+function apache_folders_debian {
+
+read -p "Do you want to do a complete back-up (A) or choose what you want to back-up (B)? ";
+if [ $REPLY == "A" ]; then
+#TODO CDW
+    echo Backing up every folder...;
+	cp -r /var/www $DIR;
+	cp -r /etc/apache2 $DIR;
+	cp -r /var/lib/mysql $DIR;
+	cp -r /etc/ssl/certs $DIR;
+	cp -r /run/apache2 $DIR;
+	cp -r /var/run/apache2 $DIR;
+fi
+
+else 
+	apache_folders_debian_choose
+}
+
+
+
+
+
+function apache_folders_debian_choose {
 
 read -p "Do you want to backup folder /var/www? ";
 if [ $REPLY == "y" ]; then
@@ -124,11 +149,159 @@ if [ $REPLY == "y" ];
 fi
 }
 
-# Kwinten doet OS
-check_apache
+function apache_folders_ubuntu {
+
+read -p "Do you want to do a complete back-up (A) or choose what you want to back-up (B)? ";
+if [ $REPLY == "A" ]; then
+#TODO CDW
+    echo Backing up every folder...;
+	cp -r /var/www $DIR;
+	cp -r /etc/httpd $DIR;
+	cp -r /var/lib/mysql $DIR;
+	cp -r /etc/ssl/certs $DIR;
+	cp -r /run/httpd $DIR;
+	cp -r /var/run/httpd $DIR;
+fi
+
+else 
+	apache_folders_ubuntu_choose
+	
+}
+
+function apache_folders_ubuntu_choose {
+
+read -p "Do you want to backup folder /var/www? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /var/www $DIR;
+fi
+
+read -p "Do you want to backup folder /etc/httpd? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /etc/httpd $DIR;
+fi
+
+read -p "Do you want to backup folder /var/lib/mysql? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /var/lib/mysql $DIR;
+fi
+
+read -p "Do you want to backup folder /etc/ssl/certs? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /etc/ssl/certs $DIR;
+fi
+
+read -p "Do you want to backup folder /run/httpd? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /run/httpd $DIR;
+fi
+
+read -p "Do you want to backup folder /var/run/httpd? ";
+if [ $REPLY == "y" ]; 
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /var/run/httpd $DIR;
+fi
+}
 
 
 
+
+
+####  NGINX ####
+
+
+function nginx_folders {
+
+read -p "Do you want to do a complete back-up (A) or choose what you want to back-up (B)? ";
+if [ $REPLY == "A" ]; then
+#TODO CDW
+    echo Backing up every folder...;
+	cp -r /usr/share/nginx $DIR;
+	cp -r /etc/nginx $DIR;
+	cp -r /usr/local/nginx/conf $DIR;
+	cp -r /usr/local/etc/nginx $DIR;
+	cp -r /var/lib/mysql $DIR;
+	cp -r /etc/ssl/certs $DIR;
+	cp -r /run/nginx.pid $DIR;
+	cp -r /var/run/nginx.pid $DIR;
+fi
+
+else 
+	nginx_folders_debian_choose
+	
+}
+
+function nginx_folders_choose {
+
+read -p "Do you want to backup folder /usr/share/nginx? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /usr/share/nginx $DIR;
+fi
+
+read -p "Do you want to backup folder /etc/nginx? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /etc/nginx $DIR;
+fi
+
+read -p "Do you want to backup folder /usr/local/nginx/conf? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /usr/local/nginx/conf $DIR;
+fi
+
+read -p "Do you want to backup folder /usr/local/etc/nginx? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /usr/local/etc/nginx $DIR;
+fi
+
+read -p "Do you want to backup folder /var/lib/mysql? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /var/lib/mysql $DIR;
+fi
+
+read -p "Do you want to backup folder /etc/ssl/certs? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /etc/ssl/certs $DIR;
+fi
+
+read -p "Do you want to backup folder /run/nginx.pid? ";
+if [ $REPLY == "y" ]; then
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /run/nginx.pid $DIR;
+fi
+
+read -p "Do you want to backup folder /var/run/nginx.pid? ";
+if [ $REPLY == "y" ]; 
+#TODO CDW
+    echo Backing up folder...;
+	cp -r /var/run/nginx.pid $DIR;
+fi
+}
+
+
+check_webapp
 
 ##############################
 # DO NOT ADD CHANGES BELOW! #
